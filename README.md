32111b0a21c8578e774a54b113760b69e065dd52
Rafael Pereira da Silva <rafael6braga@gmail.com>
2023-09-07 21:21:49 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# Quantum Circuits with Qiskit
+
+This jupyter notebook is a supplementary resource to the author’s working paper  _Quantum Computing: An Overview_. The paper aims to provide an accessible overview of quantum computing. We give high-level explanations of the key quantum properties that are leveraged by a quantum computer, along with the quantum mathematical formalism involved and references for further reading.
+
+The notebook has the following sections:
+  
+   1. One qubit circuit
+   
+       2.1. Quantum Coin Game
+       
+   2. Bell states circuit
+   
+       2.1. Run on a quantum simulator
+       
+       2.2. Run on a real quantum processor
+
+
+## Installation
+
+
+
+Install requirements locally (ideally, in a virtual environment):
+
+    pip install -r requirements.txt
+
+
+
+To run the notebook:
+
+```bash
+jupyter notebook
+```
+
+<!--- [^1]: Leap's IDE, which runs VS Code, does not support all notebook extensions. --->
+
+## References
+
+Pereira da Silva, R. (2023), [_Quantum Computing: An Overview_](http://dx.doi.org/10.2139/ssrn.4347584) SSRN Electronic Journal
+ 
+## License
+
+Released under the Apache License 2.0. See LICENSE file.
---
+++ tests/test_jn.py
@@ -0,0 +1,60 @@
+import os
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
+import unittest
+
+def run_jn(jn, timeout):
+
+    open_jn = open(jn, "r", encoding='utf-8')
+    notebook = nbformat.read(open_jn, nbformat.current_nbformat)
+    open_jn.close()
+
+    preprocessor = ExecutePreprocessor(timeout=timeout, kernel_name='python3')
+    preprocessor.allow_errors = True
+    preprocessor.preprocess(notebook, {'metadata': {'path': os.path.dirname(jn)}})
+
+    return notebook
+
+def collect_jn_errors(nb):
+
+    errors = []
+    for cell in nb.cells:
+        if 'outputs' in cell:
+            for output in cell['outputs']:
+                if output.output_type == 'error':
+                    errors.append(output)
+
+    return errors
+
+def embedding_fail(error_list):
+    return error_list and error_list[0].evalue == 'no embedding found'
+
+def robust_run_jn(jn, timeout, retries):
+
+    run_num = 1
+    notebook = run_jn(jn, timeout)
+    errors = collect_jn_errors(notebook)
+
+    while embedding_fail(errors) and run_num < retries:
+        run_num += 1
+        notebook = run_jn(jn, timeout)
+        errors = collect_jn_errors(notebook)
+
+    return notebook, errors
+
+def cell_text(nb, cell):
+    return nb["cells"][cell]["outputs"][0]["text"]
+
+jn_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+jn_file = os.path.join(jn_dir, '01-factoring-overview.ipynb')
+
+class TestJupyterNotebook(unittest.TestCase):
+    @unittest.skipIf(os.getenv('SKIP_INT_TESTS'), "Skipping integration test.")
+    def test_jn(self):
+        # Smoketest
+        MAX_EMBEDDING_RETRIES = 3
+        MAX_RUN_TIME = 100
+
+        nb, errors = robust_run_jn(jn_file, MAX_RUN_TIME, MAX_EMBEDDING_RETRIES)
+
+        self.assertEqual(errors, [])
